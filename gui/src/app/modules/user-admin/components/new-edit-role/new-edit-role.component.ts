bd82544872bad24a760003e74511c3fb56a3a34f
vickykatoch <vickykatoch@yahoo.com>
2018-09-25 22:27:49 -0400

    chnages

--- gui/src/app/modules/user-admin/components/new-edit-role/new-edit-role.component.ts
+++ gui/src/app/modules/user-admin/components/new-edit-role/new-edit-role.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Resource } from '../../store/models';
+import { Observable, empty } from 'rxjs';
 
 @Component({
   selector: 'avam-new-edit-role',
@@ -18,14 +20,23 @@ export class NewEditRoleComponent implements OnInit {
     this.roleID = value;
   }
 
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
     this.roleFormGroup = this.fb.group({
-
+      name: ['', Validators.required],
+      isAdmin : false,
+      isActive: true
     });
   }
 
+  ngOnInit() {
+
+  }
+  onResourceSelected(resource: Resource) {
+
+  }
+  onSearch(query: string) : Observable<Resource[]> {
+    return empty();
+  }
   saveRole(e: Event) {
 
   }
