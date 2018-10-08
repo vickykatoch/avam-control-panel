c28c515fa7b1b042d614345cde7c511cffee68bf
Balwinder Katoch <balwinderkatoch@MacBook-Pro.local>
2018-10-08 11:26:50 -0400

    changes

--- server/api/admin/routes/resource-routes.js
+++ server/api/admin/routes/resource-routes.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 const resourceController = require('../controllers/resource-controller');
 
 router.get('/', resourceController.fetchAll);
-router.get('/:id', resourceController.fetchById);
 router.post('/new', resourceController.addEntity);
 router.put('/update', resourceController.updateEntity);
 router.delete('/remove', resourceController.removeEntity);
 router.post('/roles',resourceController.fetchByRoles);
+router.get('/query', resourceController.query);
+router.get('/:id', resourceController.fetchById);
 
 module.exports = router;
--- server/api/admin/routes/role-routes.js
+++ server/api/admin/routes/role-routes.js
@@ -4,8 +4,9 @@ const roleController = require('../controllers/role-controller');
 router.get('/', roleController.fetchAll);
 router.get('/:id', roleController.fetchById);
 // router.get('/findbyName', roleController.findbyName);
-router.post('/new', roleController.addEntity);
-router.put('/update', roleController.updateEntity);
+// router.post('/new', roleController.addEntity);
+// router.put('/update', roleController.updateEntity);
+router.post('/save', roleController.upsertEntity);
 router.delete('/remove', roleController.removeEntity);
 
 module.exports = router;
